0fd10c622b5e17126a716406b714e0d055b8402f
George Benainous <georgesb@mac.com>
2018-06-05 00:10:45 -0700

    Update script.js

--- p5jsimage/script.js
+++ p5jsimage/script.js
@@ -1,6 +1,6 @@
 var img;
 function preload() {
-  img = loadImage('monkey.png');
+  img = loadImage('http://georgesb.github.io/p5jsimage/monkey.png');
 }
 function setup() {
   createCanvas(500,500);
